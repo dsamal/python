f10d9237c1dab7fd3278b4e2f8820ff2e32c8a32
Debasish Samal <samal.debasish@gmail.com>
2015-09-12 15:42:23 -0700

    Updated  file

--- py_condition.py
+++ py_condition.py
@@ -1,9 +1,16 @@
 __author__ = 'DEBASISH'
 #Conditions in Python
 
-if True==1:
-    print "This is True"
-elif True==4:
-    print "This is also True"
+print "Welcome! Here we will try few conditional statements in Python"
+name = raw_input("What's your Name")
+age = input("What is your Age")
+if age >= 60:
+    print "Hmmm Seems like ", name, " is a retired Person"
+elif age >= 40 & age < 60:
+    print "Hmmm Seems like ", name, " is an experienced professional"
+elif age >= 20 & age < 40:
+    print "Hmmm Seems like ", name, " is a young guy"
+elif age < 10:
+    print "Oh..", name, " is a baby"
 else:
-    print "This is False"
+    print "Oh..", name, "is a grown up boy"
